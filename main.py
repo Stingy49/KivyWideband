0f0c0654c38fea7133125cc3a45df4a6a5ed4796
Stingy49 <eellsd@gmail.com>
2017-03-17 14:35:04 -0400

    Added comments to code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Derek Eells
 # I2C Wideband Readout
+# March 2017
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -8,26 +9,28 @@ from kivy.properties import NumericProperty, StringProperty
 from kivy.clock import Clock
 from kivy.garden.graph import Graph, SmoothLinePlot
 from threading import Thread
-#from collections import deque
 
 def i2cWorker():
   # I2C data gatherer
+  # I figured I could use a seperate thread to gather my i2c data to prevent possible UI hangups
+  # Obviously I haven't implemented this code yet
   print('Starting I2C Worker')
   while True:
     #read that i2c data boiiiii
     print('poop')
 
 class WidebandWidget(Widget):
+  # Kivy property types
   h = NumericProperty(0)
   r = StringProperty("14.7")
+  # Generateing the two arrays for the graph (NOTE: This should be moved to the i2c thread when it exists)
+  # Also whenever the refresh rate or data persistence length is changed, these initializations change
   hist_t = [(t/60.0) for t in range(0,601)]
-  #print(hist_t)
   hist_v = [(14.7) for v in range(0,601)]
-  #print(hist_v)
+  # Creating the array of tuples for kivy graph
   hist = zip(hist_t, hist_v)
-  # print(hist)
-  test = 1
 
+  # For some reason the plot has to be initialized this way
   def __init__(self,):
     super(WidebandWidget, self).__init__()
     self.p = SmoothLinePlot(color=[1, 1, 1, 1])
@@ -35,8 +38,8 @@ class WidebandWidget(Widget):
 
 
   def update(self, dT):
-    # This could be conviently centered around the green value
-    # and then some abs bullshit could be used to make it go red in both directions
+    # This could be conviently centered around the ideal green value
+    # and then some abs value bullshit could be used to make it go red in both directions
     self.h = self.ids.s1.value * .0497 - .3976
     self.r = str(self.ids.s1.value)
 
@@ -44,12 +47,6 @@ class WidebandWidget(Widget):
     self.hist_v.pop(0)
     self.hist_v.append(self.ids.s1.value)
     self.hist = zip(self.hist_t, self.hist_v)
-    # if (self.test >= 1):
-    #   self.test = 1
-    # self.test += 1.0/10.0
-    # if(len(self.hist) >= 10):
-    #self.hist.pop(0)
-    #self.hist.append((self.test, self.ids.s1.value))
     self.p.points = self.hist
     
 
